a8fbb0069713499755ec3e15d7e9c3cadd4a789c
Julio Anccasi <julio.anccasi.dev@gmail.com>
2024-10-29 21:21:56 -0500

    First commit

---
+++ src/modules/projects/views/ProjectsView.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="overflow-x-auto">
+    <table class="table">
+      <!-- head -->
+      <thead>
+        <tr>
+          <th></th>
+          <th>Proyecto</th>
+          <th>Tareas</th>
+          <th>Avance</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+          v-for="(project, index) in projectsStore.projectsWithCompletion"
+          :key="project.id"
+          class="hover"
+        >
+          <th>{{ index + 1 }}</th>
+          <td>{{ project.name }}</td>
+          <td>{{ project.taskCount }}</td>
+          <td>
+            <progress
+              class="progress progress-primary w-56"
+              :value="project.completion"
+              max="100"
+            ></progress>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+
+  <input-modal
+    :open="modalOpen"
+    @close="modalOpen = false"
+    @value="projectsStore.addProject"
+    placeholder="Ingrese el nombre del proyecto"
+    title="Nuevo proyecto"
+    sub-title="Dale un nombre único a tu proyecto"
+  />
+
+  <custom-modal :open="customModalOpen">
+    <template #header>
+      <h1 class="text-3xl">Título del modal</h1>
+    </template>
+
+    <template #body>
+      <p>
+        Laboris dolor deserunt sit eu elit sit consequat cupidatat cupidatat
+        cupidatat quis cupidatat.
+      </p>
+    </template>
+
+    <template #footer>
+      <div class="flex justify-end">
+        <div class="flex justify-end mt-5">
+          <button @click="customModalOpen = false" class="btn mr-4">
+            Close
+          </button>
+          <button @click="customModalOpen = false" class="btn btn-primary">
+            Aceptar
+          </button>
+        </div>
+      </div>
+    </template>
+  </custom-modal>
+
+  <fab-button @click="modalOpen = true">
+    <AddCircle />
+  </fab-button>
+
+  <fab-button @click="customModalOpen = true" position="bottom-left">
+    <ModalIcon />
+  </fab-button>
+</template>
+
+<script lang="ts" setup>
+import CustomModal from '@/modules/common/components/CustomModal.vue';
+import FabButton from '@/modules/common/components/FabButton.vue';
+import InputModal from '@/modules/common/components/InputModal.vue';
+import AddCircle from '@/modules/common/icons/AddCircle.vue';
+import ModalIcon from '@/modules/common/icons/ModalIcon.vue';
+import { ref } from 'vue';
+import { useProjectsStore } from '../store/projects.store';
+
+const modalOpen = ref(false);
+const customModalOpen = ref(false);
+
+const projectsStore = useProjectsStore();
+</script>
